323d1ec29ea39c7d9547169860c09e6be0af8881
Imabanana80 <imabanana80.hallo@gmail.com>
2024-11-27 23:31:28 +0800

    chore: im gonna go sleep lmfao

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#ffbf00",
+                secondary: "#9b7eca",
+                accent: "#7ed4fb",
+                extra: "#8fcc75",
+                gray: "#999999",
+			}
+		},
 	},
 	plugins: [],
 }
